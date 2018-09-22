951c297765b631d326bfc3f120a332e65753630a
matterdark <johan.d.s.vonk@gmail.com>
2018-09-22 21:50:44 +0000

    YAY

---
+++ 09-20/Circle/CircleRunner.java
@@ -0,0 +1,8 @@
+public class CircleRunner {
+	public static void main (String[] args) {
+		Circle circle = new Circle();
+		circle.printArea();
+		circle.setRadius();
+		circle.printArea();
+	}
+}
---
+++ 09-21/Test/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+	public static void main (String[] args) {
+		Test test = new Test();
+		test.setVariables();
+		test.printCircumference();
+	}
+}
