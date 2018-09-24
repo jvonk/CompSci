dc0d0a61151b5450d1d11d9eea7dcd5c5aea4ce3
matterdark <johan.d.s.vonk@gmail.com>
2018-09-24 02:08:12 +0000

    Radius is added

---
+++ 09-24/Radius/Radius.java
@@ -0,0 +1,12 @@
+public class Radius {
+    private double pi = 3.14;
+    public void printArea(double radius) {
+        System.out.println("Area: "+pi*radius*radius);
+    }
+    public void printCir(double radius) {
+        System.out.println("Circumference: "+2.0*pi*);
+    }
+    public void printConeVol(double radius, double height) {
+        System.out.println("Cone Volume: "+1.0/3.0*pi*radius*radius*height);
+    }
+}
---
+++ 09-24/Radius/RadiusRunner.java
@@ -0,0 +1,8 @@
+public class RadiusRunner {
+    public static void main (String[] args) {
+        Radius rad = new Radius();
+        rad.printArea(10);
+        rad.printCir(10);
+        rad.printConeVol(10,5);
+    }
+}
