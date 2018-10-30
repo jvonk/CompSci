8123af8ec1388f993ba6fb622faf87cc97551ebd
matterdark <johan.d.s.vonk@gmail.com>
2018-10-30 16:21:21 +0000

    stuff

--- 11-01/WarmUp/Runner.java
+++ 11-01/WarmUp/Runner.java
@@ -1,5 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
-        
+        WarmUp w = new WarmUp(5);
+        w.count();
+        System.out.println(w.getFactorial());
     }
 }
--- 11-01/WarmUp/WarmUp.java
+++ 11-01/WarmUp/WarmUp.java
@@ -1,3 +1,17 @@
 public class WarmUp {
-    
+    int num;
+    public WarmUp (int in) {
+        num=in;
+    }
+    public void count() {
+        int c = 1;
+        while (c <= num)
+            System.out.println(c++);
+    }
+    public int getFactorial() {
+        int res = 1, i = 1;
+        while ( i <= num )
+            res*=i++;
+        return res;
+    }
 }
