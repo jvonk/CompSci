a546521a66f081f117cfc02490ef74976e2e1efb
Johan Vonk <johan.d.s.vonk@gmail.com>
2019-01-16 19:18:07 -0800

    minor improvements

--- 01-17/ContactListManagerLab/Screen.java
+++ 01-17/ContactListManagerLab/Screen.java
@@ -6,13 +6,12 @@ import java.util.*;
 import java.util.List;
 
 public class Screen extends JPanel implements ActionListener {
-    private List<Contact> contactList;
-    private List<Contact> contacts;
+    private List<Contact> contactList, contactsSorted;
     private JTextPane allContactsPane, searchResultsPane;
-    private JButton searchFirstButton, searchLastButton, searchUserButton, searchDomainButton, searchExtensionButton, searchByFirstName, searchByLastName, searchByEmail, addContact;
-    private JButton lastButton = searchFirstButton;
+    private JButton searchFirstButton, searchLastButton, searchUserButton, searchDomainButton, searchExtensionButton, searchByFirstName, searchByLastName, searchByEmail, addContactButton, lastButton;
     private JTextField searchInput, firstNameInput, lastNameInput, emailInput;
     private Comparator sort = Contact.Comparators.LASTNAME;
+    
     public Screen() {
         this.setLayout(null);
         contactList = new ArrayList<Contact>();
@@ -23,63 +22,63 @@ public class Screen extends JPanel implements ActionListener {
         contactList.add(new Contact("Elizabeth", "Smith", "elizabeth.adrian.smith@texas.edu"));
         int width = (int)getPreferredSize().getWidth();
         int height = (int)getPreferredSize().getHeight();
-        int topheight = 170;
         allContactsPane = new JTextPane();
-        allContactsPane.setBounds(10, topheight, width/2-15, height-topheight-10);
-        this.add(allContactsPane);
+        allContactsPane.setBounds(10, 170, width/2-15, height-214);
         String contactStr = "";
         for (int i = 0; i < contactList.size(); i++) {
             contactStr += contactList.get(i) + "\n";
         }
+        allContactsPane.setEditable(false);
         allContactsPane.setText(contactStr);
+        this.add(allContactsPane);
 
         searchResultsPane = new JTextPane();
-        searchResultsPane.setBounds(width/2+5, topheight, width/2-15, height-topheight-10);
+        searchResultsPane.setBounds(width/2+5, 170, width/2-15, height-214);
+        searchResultsPane.setEditable(false);
         this.add(searchResultsPane);
-        searchResultsPane.setText(contactStr);
         
-        int buttonwid = (width-10)/5;
+        int sectionWidth = (width-10)/5;
         
         searchFirstButton = new JButton("Search First Name");
-        searchFirstButton.setBounds(10, 50, buttonwid-10, 30);
+        searchFirstButton.setBounds(10, 50, sectionWidth-10, 30);
         searchFirstButton.addActionListener(this);
         this.add(searchFirstButton);
 
         searchLastButton = new JButton("Search Last Name");
-        searchLastButton.setBounds(buttonwid+10, 50, buttonwid-10, 30);
+        searchLastButton.setBounds(sectionWidth+10, 50, sectionWidth-10, 30);
         searchLastButton.addActionListener(this);
         this.add(searchLastButton);
 
         searchUserButton = new JButton("Search User Name");
-        searchUserButton.setBounds(buttonwid*2+10, 50, buttonwid-10, 30);
+        searchUserButton.setBounds(sectionWidth*2+10, 50, sectionWidth-10, 30);
         searchUserButton.addActionListener(this);
         this.add(searchUserButton);
 
         searchDomainButton = new JButton("Search Domain Name");
-        searchDomainButton.setBounds(buttonwid*3+10, 50, buttonwid-10, 30);
+        searchDomainButton.setBounds(sectionWidth*3+10, 50, sectionWidth-10, 30);
         searchDomainButton.addActionListener(this);
         this.add(searchDomainButton);
 
         searchExtensionButton = new JButton("Search Domain Extension");
-        searchExtensionButton.setBounds(buttonwid*4+10, 50, buttonwid-10, 30);
+        searchExtensionButton.setBounds(sectionWidth*4+10, 50, sectionWidth-10, 30);
         searchExtensionButton.addActionListener(this);
         this.add(searchExtensionButton);
         
         
-        buttonwid = (width-10)/3;
+        sectionWidth = (width-10)/3;
         
         searchByFirstName = new JButton("Sort By First Name");
-        searchByFirstName.setBounds(10, 90, buttonwid-10, 30);
+        searchByFirstName.setBounds(10, 90, sectionWidth-10, 30);
         searchByFirstName.addActionListener(this);
         this.add(searchByFirstName);
 
         searchByLastName = new JButton("Sort By Last Name");
-        searchByLastName.setBounds(buttonwid+10, 90, buttonwid-10, 30);
+        searchByLastName.setBounds(sectionWidth+10, 90, sectionWidth-10, 30);
         searchByLastName.addActionListener(this);
         this.add(searchByLastName);
 
         searchByEmail = new JButton("Sort By Email");
-        searchByEmail.setBounds(buttonwid*2+10, 90, buttonwid-10, 30);
+        searchByEmail.setBounds(sectionWidth*2+10, 90, sectionWidth-10, 30);
         searchByEmail.addActionListener(this);
         this.add(searchByEmail);
         
@@ -88,25 +87,26 @@ public class Screen extends JPanel implements ActionListener {
         this.add(searchInput);
 
         
-        buttonwid = (width-10)/4;
+        sectionWidth = (width-10)/4;
 
         firstNameInput = new JTextField();
-        firstNameInput.setBounds(10, 130, buttonwid-10, 30);
+        firstNameInput.setBounds(10, 130, sectionWidth-10, 30);
         this.add(firstNameInput);
 
         lastNameInput = new JTextField();
-        lastNameInput.setBounds(10+buttonwid, 130, buttonwid-10, 30);
+        lastNameInput.setBounds(10+sectionWidth, 130, sectionWidth-10, 30);
         this.add(lastNameInput);
 
         emailInput = new JTextField();
-        emailInput.setBounds(10+buttonwid*2, 130, buttonwid-10, 30);
+        emailInput.setBounds(10+sectionWidth*2, 130, sectionWidth-10, 30);
         this.add(emailInput);
 
-        addContact = new JButton("Add Contact");
-        addContact.setBounds(10+buttonwid*3, 130, buttonwid-10, 30);
-        addContact.addActionListener(this);
-        this.add(addContact);
-
+        addContactButton = new JButton("Add Contact");
+        addContactButton.setBounds(10+sectionWidth*3, 130, sectionWidth-10, 30);
+        addContactButton.addActionListener(this);
+        this.add(addContactButton);
+        
+        lastButton = searchFirstButton;
         
         this.setFocusable(true);
     }
@@ -119,43 +119,43 @@ public class Screen extends JPanel implements ActionListener {
         super.paintComponent(g);
     }
     public void updateStr() {
-        contacts = new LinkedList<Contact>();
+        contactsSorted = new LinkedList<Contact>();
         String search = searchInput.getText();
         if (lastButton == searchFirstButton) {
             for (int i = 0; i < contactList.size(); i++) {
                 if (contactList.get(i).getFirstName().equals(search)) {
-                    contacts.add(contactList.get(i));
+                    contactsSorted.add(contactList.get(i));
                 }
             }
         } else if (lastButton == searchLastButton) {
             for (int i = 0; i < contactList.size(); i++) {
                 if (contactList.get(i).getLastName().equals(search)) {
-                    contacts.add(contactList.get(i));
+                    contactsSorted.add(contactList.get(i));
                 }
             }          
         } else if (lastButton == searchUserButton) {
             for (int i = 0; i < contactList.size(); i++) {
                 if (contactList.get(i).getUserName().equals(search)) {
-                    contacts.add(contactList.get(i));
+                    contactsSorted.add(contactList.get(i));
                 }
             }
         } else if (lastButton == searchDomainButton) {
             for (int i = 0; i < contactList.size(); i++) {
                 if (contactList.get(i).getDomainName().equals(search)) {
-                    contacts.add(contactList.get(i));
+                    contactsSorted.add(contactList.get(i));
                 }
             }
         } else if (lastButton == searchExtensionButton) {
             for (int i = 0; i < contactList.size(); i++) {
                 if (contactList.get(i).getDomainExtension().equals(search)) {
-                    contacts.add(contactList.get(i));
+                    contactsSorted.add(contactList.get(i));
                 }
             }
         }
-        Collections.sort(contacts, sort);
+        Collections.sort(contactsSorted, sort);
         String contactStr = "";
-        for (int i = 0; i < contacts.size(); i++) {
-            contactStr += contacts.get(i) + "\n";
+        for (int i = 0; i < contactsSorted.size(); i++) {
+            contactStr += contactsSorted.get(i) + "\n";
         }
         searchResultsPane.setText(contactStr);
     }
@@ -168,7 +168,7 @@ public class Screen extends JPanel implements ActionListener {
             sort = Contact.Comparators.LASTNAME;
         } else if (e.getSource() == searchByEmail) {
             sort = Contact.Comparators.EMAIL;
-        } else if (e.getSource() == addContact) {
+        } else if (e.getSource() == addContactButton) {
             if (contactList.size()>=10) return;
             contactList.add(new Contact(firstNameInput.getText(), lastNameInput.getText(), emailInput.getText()));
             String contactStr = "";
